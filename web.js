6a927a169571e5f91d0d90ec2783c8ae9f6b85fb
utente <utente@netbook31.didattica.iismarchesi.it>
2013-07-09 23:15:59 +0200

    fourth attempt

--- web.js
+++ web.js
@@ -5,9 +5,9 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 var fs = require('fs');
 var homepage = fs.readFileSync('index.html');
-var buf = new Buffer (homepage);
-response.send(buf.toString('utf8', 0, buf.length))});
-});
+//var buf = new Buffer (homepage);
+//response.send(buf.toString('utf8', 0, buf.length))});
+response.send(homepage.toString('utf8'))});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
